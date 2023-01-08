e91d6e1c62c46f3e4da318a9c222db9521e00860
Hadi Kattan <92649946+HadiKattan@users.noreply.github.com>
2023-01-08 15:52:44 +0200

    Add files via upload

---
+++ trash_can.cpp
@@ -0,0 +1,43 @@
+#define ECHO_PIN 2
+#define TRIG_PIN 3
+int servoPin = 6;
+#include <Servo.h>
+Servo s;
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  s.attach(servoPin);
+  s.write(0);
+}
+
+float readDistanceCM() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  long duration = pulseIn(ECHO_PIN, HIGH);
+  return duration * 0.034 / 2;
+}
+
+void loop() {
+ float distance = readDistanceCM();
+  if(distance > 0 && distance <= 20){
+    if(s.read() <= 65)
+    s.write(65);
+    Serial.println(s.read());
+  }
+  else if(distance >= 30){
+    if(s.read() == 65){
+      for(int i = 65;i>=0;i-=4){
+        distance = readDistanceCM();
+        if(distance <= 20)
+        break;
+        s.write(i);
+        delay(100);
+      }
+    }
+  }
+}
